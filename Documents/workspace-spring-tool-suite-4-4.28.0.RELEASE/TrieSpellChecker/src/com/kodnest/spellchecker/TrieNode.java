2168e6a6c99f4a1b190af64003651352a5022f51
Jaishankar Kumar <k.kumarjaishankar@gmail.com>
2025-03-03 00:40:49 +0530

    Delete Documents/workspace-spring-tool-suite-4-4.28.0.RELEASE/TrieSpellChecker directory

--- Documents/workspace-spring-tool-suite-4-4.28.0.RELEASE/TrieSpellChecker/src/com/kodnest/spellchecker/TrieNode.java
+++
@@ -1,9 +0,0 @@
-package com.kodnest.spellchecker;
-
-import java.util.HashMap;
-import java.util.Map;
-
-class TrieNode {
-    Map<Character, TrieNode> children = new HashMap<>();
-    boolean endOfWord;
-}
